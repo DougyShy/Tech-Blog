05c93cbcdd69a1683a7e23fb512092d81245ef10
Doug Scheible <doug_scheible@yahoo.com>
2024-05-02 21:31:28 -0500

    figured it out yet?

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+//DATABASE_URL is what my secret was named but it could be different for you
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
