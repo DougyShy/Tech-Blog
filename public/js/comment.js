06e560b6f978b0ebebca78cf2f3e69b703b401f5
Doug Scheible <doug_scheible@yahoo.com>
2024-05-13 01:14:48 -0500

    comment ability added

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,10 +2,11 @@ const contentFormHandler = async (event) => {
     event.preventDefault();
 
     const content = document.querySelector('#comment-content').value.trim();
+    const id = event.target.getAttribute('data-id');
 
     if (content) {
         // Send a POST request to the API endpoint
-        const response = await fetch('/api/comments', {
+        const response = await fetch(`/api/comments/${id}`, {
           method: 'POST',
           body: JSON.stringify({ content }),
           headers: { 'Content-Type': 'application/json' },
